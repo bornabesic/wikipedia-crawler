54c4a37b389b19d9f7edea26534b2a98068e0bc3
Borna <besic.borna@gmail.com>
2017-03-13 14:09:14 +0100

    Refactor code and optimize for summary collection

--- crawler.py
+++ crawler.py
@@ -1,6 +1,4 @@
-import wikipedia
-import time
-import os
+from wikipedia import Wikipedia
 import argparse
 
 ''' A script used to randomly collect Wikipedia articles '''
@@ -28,31 +26,7 @@ parser.add_argument(
 
 args = parser.parse_args()
 
-''' Create directory where data will be saved '''
-
-try:
-	os.mkdir("./data/")
-except FileExistsError:
-	pass
-
 ''' Start crawling '''
 
-start = time.time()
-while True:
-	title = wikipedia.random_title(subdomain=args.subdomain)
-	clean_title = "".join(c for c in title if c.isalnum())
-	path = "./data/"+clean_title+".txt"
-	if os.path.exists(path):
-		continue
-
-	content = wikipedia.get(title, summary=args.summary, subdomain=args.subdomain)
-	if content==None or content=="":
-		continue
-
-	with open(path, "wt", encoding="utf-8") as f:
-		f.write(content)
-
-	end = time.time()
-	if end-start>args.time_limit:
-		break
-
+wiki = Wikipedia(args.subdomain, args.summary)
+wiki.crawl(args.time_limit)
--- wikipedia.py
+++ wikipedia.py
@@ -1,32 +1,89 @@
 import json
 import urllib.request
 import sys
+import time
+import os
 
-def random_title(subdomain):
-	''' Returns the title of a random Wikipedia article '''
-	url = "https://"+subdomain+".wikipedia.org/w/api.php?format=json&action=query&list=random&rnnamespace=0"
-	json_doc = urllib.request.urlopen(url).read().decode("utf-8")
-	parsed = json.loads(json_doc)
-	return parsed["query"]["random"][0]["title"]
+class Wikipedia:
 
-def get(title, summary, subdomain):
-	''' Returns full or summarized Wikipedia article specified by given name '''
-	if title==None or title=="":
-		return None
+	def __init__(self, subdomain="en", summary=True):
+		self.subdomain = subdomain
+		self.summary = summary
 
-	if summary:
-		summary_param="&exintro"
-	else:
-		summary_param=""
+	def random_article_titles(self, num_of_articles=20):
+		''' Returns titles of random Wikipedia articles '''
 
-	url = "https://"+subdomain+".wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exlimit=max&explaintext&redirects"+summary_param+"&titles="+urllib.parse.quote_plus(title)
-	json_doc = urllib.request.urlopen(url).read().decode("utf-8")
-	parsed = json.loads(json_doc)
-	pages = parsed["query"]["pages"]
+		url = "https://"+self.subdomain+".wikipedia.org/w/api.php?format=json&action=query&list=random&rnnamespace=0&rnlimit="+str(num_of_articles)
+		json_doc = urllib.request.urlopen(url).read().decode(encoding="utf-8", errors="ignore")
+		parsed = json.loads(json_doc)
+		titles=[]
+		for article in parsed["query"]["random"]:
+			titles.append(article["title"])
+		return titles
 
-	try:
-		for i in pages:
-			return pages[i]["extract"].encode(sys.stdout.encoding, errors="replace").decode(sys.stdout.encoding).strip()
-	except KeyError:
-		return None
+
+	def get(self, titles):
+		''' Returns full or summarized Wikipedia articles specified by their titles '''
+
+		if titles==None or len(titles)<1:
+			return None
+
+		articles_dict=dict()
+
+		if self.summary:
+			titles_string="|".join(titles)
+			url = "https://"+self.subdomain+".wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exlimit=max&explaintext&redirects&exintro&titles="+urllib.parse.quote_plus(titles_string)
+			json_doc = urllib.request.urlopen(url).read().decode(encoding="utf-8", errors="ignore")
+			parsed = json.loads(json_doc)
+			pages = parsed["query"]["pages"]
+
+			for i in pages:
+				page = pages[i]
+				title=page["title"].encode(encoding="utf-8", errors="ignore").decode(encoding="utf-8")
+				content=page["extract"].encode(encoding="utf-8", errors="ignore").decode(encoding="utf-8")
+				articles_dict[title]=content
+
+		else:
+			for title in titles:
+				url = "https://"+self.subdomain+".wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exlimit=max&explaintext&redirects&titles="+urllib.parse.quote_plus(title)
+				json_doc = urllib.request.urlopen(url).read().decode("utf-8", errors="ignore")
+				parsed = json.loads(json_doc)
+				pages = parsed["query"]["pages"]
+
+				for i in pages:
+					page = pages[i]
+					title=page["title"].encode(encoding="utf-8", errors="ignore").decode(encoding="utf-8")
+					content=page["extract"].encode(encoding="utf-8", errors="ignore").decode(encoding="utf-8")
+					articles_dict[title]=content
+
+		return articles_dict
+
+
+	def crawl(self, time_limit):
+		''' Crawls Wikipedia for the specified amount of time in seconds '''
+
+		try:
+			os.mkdir("./data/")
+		except FileExistsError:
+			pass
+
+		start = time.time()
+		while True:
+			titles = self.random_article_titles()
+			articles = self.get(titles)
+
+			for title in articles:
+				clean_title = "".join(c for c in title if c.isalnum())
+				path = "./data/"+clean_title+".txt"
+				if os.path.exists(path):
+					continue
+
+				with open(path, "wt", encoding="utf-8") as f:
+					f.write(articles[title])
+
+			end = time.time()
+			if end-start>time_limit:
+				break
+
+		
 
